17547effaab296c1a85a126f023c3930f638fcda
John Deng <john.deng@qq.com>
2021-01-07 18:27:31 +0800

    adjusted kube sample

--- samples/platform/kube/run.sh
+++ samples/platform/kube/run.sh
@@ -24,21 +24,21 @@ gateway_upstreams="http://payment:8080,http://order:8080,http://user:8080,http:/
 
 new_app api-gateway v1 cluster01 "${gateway_upstreams}" "${GW_NODE_PORT}"
 
-new_app order v1 cluster01 "http://payment:8080,http://user:8080,http://reviews:8080,http://inventory:8080,"
-new_app order v2 cluster01 "http://payment:8080,http://user:8080,http://reviews:8080,http://inventory:8080,"
+new_app order v1 cluster01 "grpc://payment:7575,grpc://user:7575,grpc://reviews:7575,grpc://inventory:7575,"
+new_app order v2 cluster01 "grpc://payment:7575,grpc://user:7575,grpc://reviews:7575,grpc://inventory:7575,"
 
-new_app payment v1 cluster01 "http://user:8080,tcp://payment-db:8585"
-new_app payment v2 cluster01 "http://user:8080,tcp://payment-db:8585"
+new_app payment v1 cluster01 "grpc://user:7575,tcp://payment-db:8585"
+new_app payment v2 cluster01 "grpc://user:7575,tcp://payment-db:8585"
 
 new_app payment-db v1 cluster01 ""
 
-new_app inventory v1 cluster01 "http://user:8080,grpc://product:7575,"
+new_app inventory v1 cluster01 "grpc://user:7575,grpc://product:7575,"
 
-new_app reviews v1 cluster01 "http://user:8080,"
-new_app reviews v2 cluster01 "http://user:8080,"
-new_app reviews v3 cluster01 "http://user:8080,"
+new_app reviews v1 cluster01 "grpc://user:7575,"
+new_app reviews v2 cluster01 "grpc://user:7575,"
+new_app reviews v3 cluster01 "grpc://user:7575,"
 
-new_app recommendation v1 cluster01 "http://user:8080,"
+new_app recommendation v1 cluster01 "grpc://user:7575,"
 
 new_app category v1 cluster01 "http://product:8080,"
 new_app category v2 cluster01 "grpc://product:7575,"
