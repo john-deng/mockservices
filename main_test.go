f326fb2d34aff713aab07926f3eb8d73276c4bff
John Deng <john.deng@qq.com>
2021-01-07 11:02:29 +0800

    clean up code

--- main_test.go
+++ main_test.go
@@ -1,41 +1,13 @@
 package main
 
 import (
-	"context"
-	"github.com/stretchr/testify/assert"
-	"hidevops.io/hiboot/examples/web/grpc/helloworld/protobuf"
+	"sync"
 	"testing"
-	"time"
 )
 
+var mu sync.Mutex
 func TestRunMain(t *testing.T) {
+	mu.Lock()
 	go main()
-
-	time.Sleep(time.Second)
-}
-
-func TestHelloServer(t *testing.T) {
-
-	serviceServer := newHelloServiceServer()
-
-	name := "Steve"
-	expected := "Hello " + name
-	req := &protobuf.HelloRequest{Name: name}
-	res, err := serviceServer.SayHello(context.Background(), req)
-
-	assert.Equal(t, nil, err)
-	assert.Equal(t, expected, res.Message)
-}
-
-func TestHolaServer(t *testing.T) {
-
-	serviceServer := newHolaServiceServer()
-
-	name := "Steve"
-	expected := "Hola " + name
-	req := &protobuf.HolaRequest{Name: name}
-	res, err := serviceServer.SayHola(context.Background(), req)
-
-	assert.Equal(t, nil, err)
-	assert.Equal(t, expected, res.Message)
+	mu.Unlock()
 }
